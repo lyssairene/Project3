e42a45d5d6a76fb7fa3f166faa2eccbdd8c9980b
Ashley Kearney <69979046+kearneyam-ai@users.noreply.github.com>
2024-04-09 10:49:26 -0400

    initialization of project
    new rust project

---
+++ P3/target/debug/deps/P3-177348b2fe45b4ce.d
@@ -0,0 +1,7 @@
+C:\Users\smkea\Documents\GitHub\Project3\P3\target\debug\deps\libP3-177348b2fe45b4ce.rmeta: src\lib.rs
+
+C:\Users\smkea\Documents\GitHub\Project3\P3\target\debug\deps\libP3-177348b2fe45b4ce.rlib: src\lib.rs
+
+C:\Users\smkea\Documents\GitHub\Project3\P3\target\debug\deps\P3-177348b2fe45b4ce.d: src\lib.rs
+
+src\lib.rs:
---
+++ P3/target/debug/deps/P3.d
@@ -0,0 +1,5 @@
+C:\Users\smkea\Documents\GitHub\Project3\P3\target\debug\deps\P3.exe: src\main.rs
+
+C:\Users\smkea\Documents\GitHub\Project3\P3\target\debug\deps\P3.d: src\main.rs
+
+src\main.rs:
